7ad642bd71d7d55730b73d44d47db4518957ccb8
Pieter <28593855@g.nwu.ac.za>
2019-09-01 17:38:11 +0200

    Add project files.

---
+++ CardPositions/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CardPositions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.liBxCards = new System.Windows.Forms.ListBox();
+            this.btnShuffle = new System.Windows.Forms.Button();
+            this.liBxShuffle1 = new System.Windows.Forms.ListBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // liBxCards
+            // 
+            this.liBxCards.FormattingEnabled = true;
+            this.liBxCards.Location = new System.Drawing.Point(12, 12);
+            this.liBxCards.Name = "liBxCards";
+            this.liBxCards.Size = new System.Drawing.Size(155, 693);
+            this.liBxCards.TabIndex = 0;
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.Location = new System.Drawing.Point(177, 725);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(155, 38);
+            this.btnShuffle.TabIndex = 1;
+            this.btnShuffle.Text = "Shuffle";
+            this.btnShuffle.UseVisualStyleBackColor = true;
+            this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
+            // 
+            // liBxShuffle1
+            // 
+            this.liBxShuffle1.FormattingEnabled = true;
+            this.liBxShuffle1.Location = new System.Drawing.Point(177, 12);
+            this.liBxShuffle1.Name = "liBxShuffle1";
+            this.liBxShuffle1.Size = new System.Drawing.Size(155, 693);
+            this.liBxShuffle1.TabIndex = 2;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(12, 725);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(155, 38);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 775);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.liBxShuffle1);
+            this.Controls.Add(this.btnShuffle);
+            this.Controls.Add(this.liBxCards);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox liBxCards;
+        private System.Windows.Forms.Button btnShuffle;
+        private System.Windows.Forms.ListBox liBxShuffle1;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ CardPositions/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* This is a test program to see how cards are moved when a deck of cards are shuffled
+ * It is used to test different kinds of shuffles
+ * Not much has been done to date
+ */
+
+namespace CardPositions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> deck = new List<string>(52);
+        private List<string> suits = new List<string>();
+        List<string> shuffleList = new List<string>();
+        int cuts = 0;
+        int matches = 0;
+
+        private void CheckMatch()       //Check deck to see if all match
+        {
+            for (int i = 0; i < shuffleList.Count(); i++)       //Check every card with original
+            {
+                if (liBxCards.Items[i] == liBxShuffle1.Items[i])        //if match, add one to match
+                {
+                    matches++;
+
+                    if (matches == 52)      //if whole deck matches
+                    {
+                        MessageBox.Show("Completed! \nIt took " + cuts + " cut(s) to complete this shuffle");
+                        matches = 0;
+                        cuts = 0;
+                    }
+                }
+            }
+
+            matches = 0;
+        }
+
+        private void SetDeck(List<string> suits)        //Set deck to unshuffled state
+        {
+            string[] numbers = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+
+            if (suits.Count == 0)
+            {
+                suits.Add(" of Spades");
+                suits.Add(" of Hearts");
+                suits.Add(" of Diamonds");
+                suits.Add(" of Clubs");
+            }
+
+            deck.Clear();
+
+            for (int i = 0; i < suits.Count(); i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    deck.Add(numbers[j] + suits[i]);
+                }
+            }
+        }
+
+        private void OrderShuffle()     //Deals four stacks of cards, one card per stack at a time, then assumes that stacks are fused after shuffle
+        {
+            int next = 1;
+            cuts++;
+            shuffleList.Clear();
+
+            for (int i = 0; i < deck.Count(); i += 4)       //Every fourth one, 13 per stack
+            {
+                shuffleList.Add(deck[i]);
+
+                if (i >= deck.Count() - 4 && next < 4)
+                {
+                    shuffleList.Add(deck[(i + 5) - 52]);
+                    i = next;
+                    next++;
+                }
+            }
+
+            if (shuffleList.Count() != 0)
+            {
+                for (int i = 0; i < shuffleList.Count(); i++)
+                {
+                    deck[i] = shuffleList[i];
+                }
+            }
+        }
+
+        public void DealShuffle ()      //Incomplete
+        {
+            int next = 1;
+            cuts++;
+            shuffleList.Clear();
+
+            for (int i = deck.Count(); i > 0; i -= 4)       //Every fourth one, 13 per stack
+            {
+                shuffleList.Add(deck[52 -i]);
+
+                if (i <= 4 && next > 48)
+                {
+                    shuffleList.Add(deck[(52 - i + 5)]);
+                    i = next;
+                    next++;
+                }
+            }
+
+            if (shuffleList.Count() != 0)
+            {
+                for (int i = 0; i < shuffleList.Count(); i++)
+                {
+                    deck[i] = shuffleList[i];
+                }
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            liBxCards.Items.Clear();
+            SetDeck(suits);
+
+            for (int i = 0; i < deck.Count(); i++)      //Add cards to listbox
+            {
+                liBxCards.Items.Add(deck[i]);
+            }
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            liBxShuffle1.Items.Clear();
+            OrderShuffle();
+            
+            for (int i = 0; i < shuffleList.Count(); i++)       //Add cards to shuffled list box
+            {
+                liBxShuffle1.Items.Add(shuffleList[i]);
+            }
+
+            CheckMatch();       //Check to see if shuffled cards match unshuffled cards
+        }
+    }
+}
